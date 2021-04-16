b3df13733b31ad5c8ededcea2eaf781322c52b4b
Siamak Sadeghianfar <1174934+siamaksade@users.noreply.github.com>
2021-04-16 14:01:42 +0200

    fixed default sa access to cicd namespace

--- demo.sh
+++ demo.sh
@@ -82,8 +82,8 @@ command.install() {
   info "Configure service account permissions for pipeline"
   oc policy add-role-to-user edit system:serviceaccount:$cicd_prj:pipeline -n $dev_prj
   oc policy add-role-to-user edit system:serviceaccount:$cicd_prj:pipeline -n $stage_prj
-  oc policy add-role-to-user system:image-puller system:serviceaccount:$dev_prj:default -n $stage_prj
-  oc policy add-role-to-user system:image-puller system:serviceaccount:$stage_prj:default -n $stage_prj
+  oc policy add-role-to-user system:image-puller system:serviceaccount:$dev_prj:default -n $cicd_prj
+  oc policy add-role-to-user system:image-puller system:serviceaccount:$stage_prj:default -n $cicd_prj
 
   info "Deploying CI/CD infra to $cicd_prj namespace"
   oc apply -f infra -n $cicd_prj
